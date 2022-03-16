d68643dd670e962b70d51f7a1e2b1bf6516c8304
haijianyang <weber.yhj@gmail.com>
2022-03-16 07:40:49 -0400

    capiv1

--- hack/version.sh
+++ hack/version.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
-# Copyright 2020 The Kubernetes Authors.
+#!/bin/bash
+
+# Copyright 2022.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
